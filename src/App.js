e23edabf5127923ac1cc3497fc95bd559d926359
Giorgio Basile <56266079+Ghita00@users.noreply.github.com>
2024-03-14 22:53:34 +0100

    scafolding project

---
+++ src/App.js
@@ -0,0 +1,7 @@
+import Home from "./screens/home/homeScreen";
+
+export default function App() {
+  return (
+    <Home></Home>
+  );
+}
---
+++ src/screens/home/homeScreen.js
@@ -0,0 +1,7 @@
+import Map from "../../components/map/mapComponent";
+
+export default function Home() {
+  return (
+    <Map></Map>
+  );
+}
